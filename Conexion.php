1f5ba215ff5cb835c3f353d044d66fe4beaf9094
developer00a <47408515+developer00a@users.noreply.github.com>
2019-02-15 15:42:35 -0600

    Update Conexion.php

--- Conexion.php
+++ Conexion.php
@@ -1,9 +1,9 @@
 <?PHP
 $servidor="localhost";
-$bd="";
+$basedatos="";
 $usuario="";
 $clave="";
-$conexion = mysqli_connect($servidor, $usuario, $clave, $bd);
+$conexion = mysqli_connect($servidor, $usuario, $clave, $basedatos);
 if (!$conexion)
 { echo "<h2>Error al establecer conexión con el servidor!!!</h2>"; exit; }			      
 ?>
