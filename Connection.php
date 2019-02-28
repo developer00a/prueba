311d123878d16a1b4129c09f5498382100973cbb
developer00a <47408515+developer00a@users.noreply.github.com>
2019-02-28 16:14:43 -0600

    Update Connection.php

--- Connection.php
+++ Connection.php
@@ -1,11 +1,18 @@
 <?php
 
 class Conexion {
-	public $host = "localhost";
+
+	public $host = "127.0.0.1";
 	public $port = "80";
-	public $db = "BD";
+	public $db = "DB";
 	public $user = "USU";
 	public $pass = "CLA";
+    
+    function Con(){
+        $conexion1 = new mysqli($this->host, $this->user, $this->pass,$this->db);
+        
+        return $conexion1;
+    }
 }
 
 ?>
