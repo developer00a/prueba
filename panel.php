3618e038d34dbdc99e5754527648617f51560537
developer00a <47408515+developer00a@users.noreply.github.com>
2019-03-13 12:32:09 -0600

    Add files via upload

---
+++ panel.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Panel</title>
+    <link rel="stylesheet" type="text/css" href="Resources/CSS/HojaGeneral.css"/> 
+</head>
+<body>
+    <div class="head">
+	USUARIOS REGISTRADOS
+    </div>
+
+</body>
+</html>
