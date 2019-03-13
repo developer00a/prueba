c76c8915bb6c175cf0a2ada6dbdd7827fb9ab56e
developer00a <47408515+developer00a@users.noreply.github.com>
2019-03-13 10:48:34 -0600

    Update main.php

--- main.php
+++ main.php
@@ -2,8 +2,9 @@
 <html>
 <head>
 	<title>Principal</title>
+	<link rel="stylesheet" type="text/css" href="Resources/CSS/HojaPrincipal.css"/> 
 </head>
-<body style="background: linear-gradient(to right, rgba(0,255,0,0), rgba(0,255,200,1));">
+<body>
 	<h1 style="color: #888;">LaraCode</h1>
 	<p><h4>Bienvenido</h4></p>
 </body>
